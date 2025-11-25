77333b80dab026f6beb7b184ba7a99f2127db345
pavel.zakharov <pavel.zakharov@exness.com>
2025-11-25 21:22:21 +0200

    updated storage plugin

--- plugin/trino-storage/src/main/java/org/ebyhr/trino/storage/StorageSplit.java
+++ plugin/trino-storage/src/main/java/org/ebyhr/trino/storage/StorageSplit.java
@@ -15,13 +15,11 @@ package org.ebyhr.trino.storage;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.collect.ImmutableMap;
 import io.airlift.slice.SizeOf;
 import io.trino.spi.HostAddress;
 import io.trino.spi.connector.ConnectorSplit;
 
 import java.util.List;
-import java.util.Map;
 
 import static java.util.Objects.requireNonNull;
 
